0f03449746a917e1323e7a00c0e23380e8bcc094
Phong <phong940253@gmail.com>
2022-06-09 04:32:00 +0700

    fix command run web admin

--- nso-admin/start_uwsgi.sh
+++ nso-admin/start_uwsgi.sh
@@ -1,6 +1,2 @@
-#!/bin/sh
-
-python manage.py makemigrations
-python manage.py migrate
-
-gunicorn NSOAdmin.wsgi:application -c gunicorn_config.py
+#!/bin/sh  
+python manage.py makemigrations;python manage.py migrate;gunicorn NSOAdmin.wsgi:application -c gunicorn_config.py

81a9c842fd2b82f1f9f205088ad23a85ac83e8e5
Phong <phong940253@gmail.com>
2022-09-02 13:48:36 +0700

    change file run bat

--- nso-core/run.bat
+++ nso-core/run.bat
@@ -1,12 +1,12 @@
-set DB_HOST=localhost
-set DB_PORT=3306
-set DB_USER=root
-set DB_PASS='01676940253'
-set DB_DATABASE=nja
-set DEBUG=false
-set REDIS_CACHE_DEFAULT=redis://redis:6379/1
-set TOPUP_CARD_API=https://api.doicard68.com/api/card-auto
-set TOPUP_CARD_API_KEY=C806331F-CC2A-4EAF-9148-B755DE567333
-set NSO_MS_API=http://45.77.129.249:443/api/topup_card
-java -server -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5000 -Xms1024M -Xmx1024M -XX:MaxHeapFreeRatio=50 -jar  target/NinjaServer.jar
+@REM set DB_HOST=localhost
+@REM set DB_PORT=3306
+@REM set DB_USER=root
+@REM set DB_PASS='01676940253'
+@REM set DB_DATABASE=nja
+@REM set DEBUG=false
+@REM set REDIS_CACHE_DEFAULT=redis://redis:6379/1
+@REM set TOPUP_CARD_API=https://api.doicard68.com/api/card-auto
+@REM set TOPUP_CARD_API_KEY=C806331F-CC2A-4EAF-9148-B755DE567333
+@REM set NSO_MS_API=http://45.77.129.249:443/api/topup_card
+java -server -Xms1024M -Xmx1024M -XX:MaxHeapFreeRatio=50 -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5000 target/NinjaServer.jar
 pause

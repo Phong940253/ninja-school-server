02157c80737bac9a4f0e1eca716fea1d7fcacae2
Phong <phong940253@gmail.com>
2022-09-17 23:12:26 +0700

    update heap application

--- nso-core/run.bat
+++ nso-core/run.bat
@@ -8,5 +8,5 @@
 @REM set TOPUP_CARD_API=https://api.doicard68.com/api/card-auto
 @REM set TOPUP_CARD_API_KEY=C806331F-CC2A-4EAF-9148-B755DE567333
 @REM set NSO_MS_API=http://45.77.129.249:443/api/topup_card
-java -server -Xms1024M -Xmx1024M -XX:MaxHeapFreeRatio=50 -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5000 target/NinjaServer.jar
+java -server -Xms192M -Xmx192M -XX:MaxHeapFreeRatio=50 -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5000 target/NinjaServer.jar
 pause
